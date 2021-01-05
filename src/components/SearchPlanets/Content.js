2c6fc864cecd276102dab3ad7cea62e2ec0272e0
sidhantmathur <sidhant185@gmail.com>
2021-01-05 10:57:37 -0600

    add error message for algolia limit

--- src/components/SearchPlanets/Content.js
+++ src/components/SearchPlanets/Content.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Hits } from 'react-instantsearch-dom'
 import { connectStateResults } from 'react-instantsearch/connectors'
 import Hit from './Hit.js'
+import { Text } from '@chakra-ui/react'
 export default connectStateResults(
   ({ searchState, searchResults }) =>
     searchResults && searchResults.nbHits !== 0 ? (
       <Hits hitComponent={Hit} />
     ) : (
       <div>
-        No results found for <strong>{searchState.query}</strong>.
+        <Text fontSize="md">No results found for <strong>{searchState.query}</strong> or Algolia search limit was reached.</Text>
       </div>
     )
 )
